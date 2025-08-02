75c554384a6fc68166ff65125dac9970b0990e7d
RituLog <ritu.k.gupta67@gmail.com>
2025-08-02 21:29:19 +0530

    "ceiling of element using lenear and binary search"

---
+++ src/arrays/CeilSearchBinary.java
@@ -0,0 +1,41 @@
+package arrays;
+
+public class CeilSearchBinary {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 10, 10, 12, 19};
+        int target = 3;
+        int index = ceiling(arr, target);
+
+        if (index == -1)
+            System.out.println("Ceiling of " + target + " doesn't exist in array");
+        else
+            System.out.println("Ceiling of " + target + " is " + arr[index]);
+    }
+
+    // return the index of smallest no >= target
+    static int ceiling(int[] arr, int target) {
+
+        // but what if the target is greater than the greatest number in the array
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return start;
+    }
+}
---
+++ src/arrays/CeilSearchLeanear.java
@@ -0,0 +1,37 @@
+package arrays;
+
+public class CeilSearchLeanear {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 10, 10, 12, 19};
+        int target = 3;
+        int index = ceilSearch(arr, target);
+        if(index == -1)
+            System.out.println("Ceiling of " + target + " doesn't exist in array ");
+        else
+            System.out.println("ceiling of " + target + " is " + arr[index]);
+    }
+
+
+    static int ceilSearch(int[] arr , int target){
+
+        /* If x is smaller than or equal to first element,
+            then return the first element */
+        if(target <= arr[0])
+            return 0;
+
+        /* Otherwise, linearly search for ceil value */
+        for(int i = 0; i < arr.length - 1; i++)
+        {
+            if(arr[i] == target)
+                return i;
+
+            /* if x lies between arr[i] and arr[i+1] including
+            arr[i+1], then return arr[i+1] */
+            if(arr[i] < target && arr[i+1] >= target)
+                return i+1;
+        }
+        /* If we reach here then x is greater than the last element
+            of the array, return -1 in this case */
+        return -1;
+    }
+}
