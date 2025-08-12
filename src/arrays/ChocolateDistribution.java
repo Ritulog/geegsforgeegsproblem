6bd63914edb78c5efb688b1cd2076b5a02b4d2b1
RituLog <ritu.k.gupta67@gmail.com>
2025-08-12 20:24:00 +0530

    "Chocolate Distribution"

--- src/arrays/ChocolateDistribution.java
+++ src/arrays/ChocolateDistribution.java
@@ -1,10 +1,11 @@
 package arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 class ChocolateDistribution {
-    public int findMinDiff(ArrayList<Integer> arr, int m) {
+    static int findMinDiff(ArrayList<Integer> arr, int m) {
         // your code here
         Collections.sort(arr);
         int n= arr.size();
@@ -15,5 +16,11 @@ class ChocolateDistribution {
             res = Math.min(res,maxElement- minElement);
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(3, 4, 1, 9, 56, 7, 9, 12));
+        int m = 5;
+        System.out.println(findMinDiff(arr, m));
     }
 }
